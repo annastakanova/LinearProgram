be73755d3d061af9032f262ce59c3f3e2b117c32
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 01:07:59 +0300

    Update Task35a.java

--- Task35a.java
+++ Task35a.java
@@ -1,9 +1,12 @@
+//Даны натуральные числа М и N. Вывести старшую цифру дробной части и младшую цифру целой части числа M/N.
+
 public class Task35a {
     public static void main(String[] args) {
         double m = 746812;
         double n = 5021;
         double x = m / n;
-        System.out.println("x = " + x);
-        //как вывести старшую цифру дробной части и младшую - целой???
+        System.out.println("m/n = " + x + "\n");
+        System.out.println("младшая цифра целой части = " + ((int)x%10));
+        System.out.println("старшая цифра дробной части = " + (int)(10*x)%10);
     }
 }
