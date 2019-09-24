a67dea05a03bb25ddb25298dc70a928368ad9603
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 01:03:47 +0300

    Update Task24a.java

--- Task24a.java
+++ Task24a.java
@@ -1,16 +1,22 @@
+//Найти площадь равнобедренной трапеции
+//с основаниями а и b и углом α при большем основании а.
+
+
+// s = ((a+b)*h)/2
+// h = tgA*(b-a)/2
+// s = ((a+b)*tgA*(b-a))/4 = tgA * (a^2 - b^2) / 4
+
 public class Task24a {
     public static void main(String[] args) {
-
-//        int a = 7;
-//        int b = 6;
-//        int angle = 30;
-//        if (a>b || a>0 || b>0){
-//            double s = //?????????????????????????????????
-//
-//            System.out.println("площадь трапеции = " + s);
-//        }
-//        else
-//            System.out.println("неверная длина оснований");
+        int a = 7;
+        int b = 5;
+        int angle = 1;  //угол в радианах
+        if (a>b && a>0 && b>0){
+            double s = Math.tan(angle) * (a*a - b*b) / 4;
+            System.out.println("площадь трапеции = " + s);
+        }
+        else
+            System.out.println("неверная длина оснований");
 
     }
 }
