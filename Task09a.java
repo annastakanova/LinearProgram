1294cd416cc937f21d0a774e622c591287d23162
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 00:59:05 +0300

    Update Task09a.java

--- Task09a.java
+++ Task09a.java
@@ -1,11 +1,11 @@
-//  часть формулы можно сократить, так как (a/c)*(b/d) = (a*b)/(c*d)
+//Вычислить значение выражения по формуле (все переменные принимают действительные значения:
+// (a/c) * (b/d) - (ab - c)/cd
+
+//  формулу можно сократить, так как (a/c)*(b/d) = (a*b)/(c*d)
 //  а также  с/(с*d) = 1/d
 
 public class Task09a {
     public static void main(String[] args) {
-//        double a = 9.0;
-//        double b = 4.0;
-//        double c = 3.0;
         double d = 2.0;
         double formula = 1/d;
         System.out.println("значение формулы = " + formula);
