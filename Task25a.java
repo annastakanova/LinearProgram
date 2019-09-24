2a11a3367c56b52319932ef5ad7ed3206f9b14f0
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 01:04:05 +0300

    Update Task25a.java

--- Task25a.java
+++ Task25a.java
@@ -1,3 +1,6 @@
+//Вычислить корни квадратного уравнения ах2+ bх + с = 0 с заданными коэффициентами a, b и с
+// (предполагается, что а≠0 и что дискриминант уравнения неотрицателен).
+
 public class Task25a {
     public static void main(String[] args) {
         double a = 1;
@@ -5,7 +8,7 @@ public class Task25a {
         double c = 12;
         double dis = b*b - 4*a*c;
 
-        if (a!=0 || dis>=0){
+        if (a!=0 && dis>=0){
             double x1 = (- b + Math.sqrt(dis))/(2*a);
             double x2 = (- b - Math.sqrt(dis))/(2*a);
             System.out.println("x1 = " + x1);
