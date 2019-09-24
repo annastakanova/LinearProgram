b5c473776dd0054ea65c162c083dd341ea161dfc
annastakanova <50991493+annastakanova@users.noreply.github.com>
2019-09-25 01:01:20 +0300

    Update Task16a.java

--- Task16a.java
+++ Task16a.java
@@ -1,7 +1,9 @@
+//Найти произведение цифр заданного четырехзначного числа.
+
 public class Task16a {
     public static void main(String[] args) {
         int n = 2345;
-        int s = 0, m = 1;
+        int m = 1;
         for (int i = 0; i < 4; i++) {
             m *= n % 10;
             n /= 10;
